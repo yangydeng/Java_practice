290324d639f3770e6f77820aa3ec536d083b150d
yangydeng <yangydeng@163.com>
2017-03-26 16:00:30 +0800

    recursion

---
+++ recursion/src/recursion.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class recursion {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter a number triangel:"+triangel(input.nextInt()));
+		System.out.println("Enter a number factorial:"+factorial(input.nextInt()));
+
+	}
+
+	public static int triangel(int n) {
+		if(n==1){
+			return 1;
+		}
+		else {
+			return (n+triangel(n-1));
+		}
+		
+	}
+	
+	public static int factorial(int n) {
+		if(n==1)
+			return 1;
+		else {
+			return(n*factorial(n-1));
+		}
+	}
+}
